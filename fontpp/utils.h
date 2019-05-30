04391e955f745aeaecbf13743de5a747a5434453
Nikolay Ivanov <nikolay.ivanov@egt-bg.com>
2019-05-30 20:42:15 +0300

    cleanup

--- fontpp/utils.h
+++ fontpp/utils.h
@@ -9,6 +9,7 @@
 #include <limits>
 #include <vector>
 #include <algorithm>
+#include <memory>
 
 namespace fnt
 {
@@ -38,6 +39,26 @@ static inline uint32_t upper_power_of_two(uint32_t v)
 	v |= v >> 16;
 	v++;
 	return v;
+}
+
+static inline uint32_t estimate_width(uint32_t max, uint32_t min, uint32_t surface_sqrt)
+{
+    if(max < min)
+    {
+        return max;
+    }
+
+    uint32_t width = max;
+    while(width > min)
+    {
+        if(surface_sqrt >= width * 0.7f)
+        {
+            return width;
+        }
+
+        width /= 2;
+    }
+    return width;
 }
 
 template <typename T>
